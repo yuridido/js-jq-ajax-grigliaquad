373186278b810ebec8784057572e67f4952e9e79
yuridido <yuridido@yahoo.it>
2020-08-27 16:27:36 +0200

    griglia

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+$(document).ready(function(){
+    for (var i = 0; i < 36; i++) {
+        var clone = $('.template .square').clone()
+        $('.container').append(clone);
+    };
+
+    // $.ajax(
+    //     {
+    //         url: "https://flynn.boolean.careers/exercises/api/random/int",
+    //         method: "GET",
+    //         success: function (data, stato) {
+    //             $("#risultati").html(data);
+    //         },
+    //         error: function (richiesta, stato, errori) {
+    //             alert("E' avvenuto un errore. " + errore);
+    //         }
+    //     }
+    // );
+
+
+
+
+});
